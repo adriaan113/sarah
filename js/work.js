d2cadd8038f2f093603c26ce786b257344e8fb17
Adriaan van der Ploeg <adriaanvanderploeg@gmail.com>
2019-12-03 12:50:00 +0100

    adjeusted fixed header z-index

---
+++ js/work.js
@@ -0,0 +1,35 @@
+const workData=[
+    {
+        img: ['img/test1.jpg','img/test2.jpg','img/test3.jpg'],
+        title:'sjaak magazine',
+        intro: 'The magazine made for Rotterdammers with a small budget.',
+        extraInfo:'Sjaak magazine is a magazine for rotterdam citizens with a small budget. The idea of this magazine is to be a source of information and inspiration for people with a low income. Through inspiring interviews, stories and events, as well as useful tips, Sjaak shows that there are opportunities to lead a beautiful and healthy life even with a limited budget.The magazine is published four times a year and is distributed through social organizations in Rotterdam, including the Salvation Army and the Food Bank Foundation.',
+        client:['Leger des Heils, Voedselbank'],
+        industry:'non-profit',
+        services:['concept', 'design', 'art direction', 'photography'],
+        year:'2018 - 2019 at Dailymilk'
+    }
+];
+
+
+
+
+
+
+const asideTest= document.querySelector('.intro-text');
+const work= document.querySelector('.home-work');
+const grid=document.querySelector('.grid');
+
+
+const menuBtn= document.querySelectorAll('.nav__link');
+const ctaBtn= document.querySelector('.cta-work');
+
+// for(let i=0;i<menuBtn.length;i++){
+    
+// }
+
+//replcace [1] for event to more easliy change to cotnact/about. Then just use a conditional
+// menuBtn[1].addEventListener('click',()=>{
+//     asideTest.style.display= 'none';
+//     work.
+// })
