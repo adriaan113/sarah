39612ff55e923a681ee499ba7a291f48951de5bd
Adriaan van der Ploeg <adriaanvanderploeg@gmail.com>
2020-03-28 15:20:46 +0100

    fixed issue with introtext pop up on resizing

--- js/main.js
+++ js/main.js
@@ -566,7 +566,12 @@ hoverChange(introGallery);
 window.addEventListener('resize', ()=>{
 
   document.body.style.backgroundColor= lightGrey;
-  introGallery.style.display= 'flex';
+
+  if(introText.style.display!== 'none'){
+    introGallery.style.display= 'flex';
+  }else{
+    introGallery.style.display= 'none';
+  }
 
 });
 
